a9a72b6b9c75f8ba50f21288e3436e01d9bad063
ShadowPaint-SP <71901885+ShadowPaint-SP@users.noreply.github.com>
2023-06-08 19:03:58 +0200

    form submit  actions

--- src/routes/chat/[slug]/+page.server.ts
+++ src/routes/chat/[slug]/+page.server.ts
@@ -1,9 +1,10 @@
-import { error, type Actions, fail } from "@sveltejs/kit";
+import { error, type Actions, fail, redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient()
 
+// here we get the existing content of the chat ( we populate it )
 export const load: PageServerLoad = async ({ params }) => {
 	console.log('params :>> ', params);
 	const chat = await db.chats.findUnique({
@@ -16,20 +17,42 @@ export const load: PageServerLoad = async ({ params }) => {
 	return { chat }
 }
 
-// here we get the existing content of the chat ( we populate it )
+// just sleep
+async function sleep(ms:number) {
+	return new Promise((resolve) => setTimeout(resolve, ms))
+}
 
+// the real thing
 export const actions: Actions = {
 	addMessage: async ({ request }) => {
 		const formData = await request.formData()
 		const message = String(formData.get('messagesend'))
+		const errors: Record<string, unknown> = {}
 
 		if(!message){
 			return fail(400, { message, missing: true })
 		}
 
-		//here i create the database entry and also make the api request
+		if (message.length < 5) {
+			return fail(400, { message, longer: true})
+		}
+
+		if(Object.keys(errors).length > 0){
+			const data = {
+				data: Object.fromEntries(formData),
+				errors
+			}
+			return fail(400, data)
+		}
+
+
+		// here i create the database entry and also make the api request
+		await sleep(2000)
 
 
 		return { success: true }
 	}
 }
+
+
+
--- src/routes/chat/[slug]/+page.svelte
+++ src/routes/chat/[slug]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { ActionData, PageData } from './$types'
+	import type { ActionData, PageData, SubmitFunction } from './$types'
 
 	export let data: PageData
 
@@ -10,9 +10,22 @@
 	/////////////////////////////////////////////////
 
 	import send from '$lib/svg/send.svg'
+	import trash from '$lib/svg/trash.svg'
 	import { enhance } from '$app/forms'
 
 	export let form: ActionData
+	let loading = false
+
+	const addMessage: SubmitFunction = (input) => {
+		// check if it is a link or a text
+		loading = true
+		return async ({ update }) => {
+			//do something when it is submitted
+
+			loading = false
+			await update()
+		}
+	}
 	//async function addMessage(event: Event) {
 	//	const formEl = event.target as HTMLFormElement
 	//	const data = new FormData(formEl) // input text
@@ -37,6 +50,9 @@
 <div class=" flex flex-col h-full">
 	<div class="flex flex-grow justify-around">
 		<div class="w-[768px] lg:max-w-3xl md:max-w-2xl grid content-between grid-flow-row-dense">
+			<pre>
+				{JSON.stringify(form, null, 2)}
+			</pre>
 			<div>
 				<div>
 					<hgroup>
@@ -84,10 +100,11 @@
 						<form
 							method="POST"
 							action="?/addMessage"
-							use:enhance
+							use:enhance={addMessage}
 							class="flex flex-row border border-base-200 rounded-lg shadow-md items-center hover:border-neutral-400"
 						>
 							<input
+								aria-disabled={loading}
 								tabindex="0"
 								type="text"
 								name="messagesend"
@@ -95,8 +112,15 @@
 								class="text flex-grow resize-none h-7 min-h-full focus:outline-none pl-4"
 							/>
 							{#if form?.missing}
-								<span class=" text-error">Please input text</span>
+								<span class="text-error">Please input text</span>
 							{/if}
+							{#if form?.longer}
+								<span class="text-error">min. 5 Words</span>
+							{/if}
+
+							<!--<button class="pl-2 h-auto active:scale-90 secondary" formaction="?/clearMessage">
+								<img src={trash} alt="trash" class="my-2" />
+							</button>-->
 							<button type="submit" class=" flex items-center active:scale-90 py-2 px-4">
 								<img src={send} alt="send" class="my-2" />
 							</button>
